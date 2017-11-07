3df356cb47fd0860448ea28f0a8e4d79847357f0
velascoDev <dracontium2@gmail.com>
2017-11-07 12:15:35 -0600

    set up react-md

--- src/index.js
+++ src/index.js
@@ -4,5 +4,13 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+import WebFontLoader from 'webfontloader';
+
+WebFontLoader.load({
+  google: {
+    families: ['Roboto:300,400,500,700', 'Material Icons'],
+  },
+});
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
